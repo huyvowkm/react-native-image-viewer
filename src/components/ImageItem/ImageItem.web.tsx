8afd41bb5e67ce3580b64a35eba2ac9aa3193556
huyvowkm <huyvo@wakumo.vn>
2025-09-08 15:11:25 +0700

    fix: remove initial scale value

--- src/components/ImageItem/ImageItem.web.tsx
+++ src/components/ImageItem/ImageItem.web.tsx
@@ -60,7 +60,7 @@ const ImageItem = ({
   const translateValue = new Animated.ValueXY(translate);
 
   const imagesStyles = getImageStyles(
-    // imageDimensions,
+    imageDimensions,
     translateValue,
     scaleValue
   );
