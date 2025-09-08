652bcd4daa4903d98747dc47bfccfb11589a0115
huyvowkm <huyvo@wakumo.vn>
2025-09-08 15:01:50 +0700

    fix: remove scale value

--- src/components/ImageItem/ImageItem.web.tsx
+++ src/components/ImageItem/ImageItem.web.tsx
@@ -56,27 +56,14 @@ const ImageItem = ({
   const imageDimensions = useImageDimensions(imageSrc);
   
   const [translate, scale] = getImageTransform(imageDimensions, SCREEN);
-  const scrollValueY = new Animated.Value(0);
   const scaleValue = new Animated.Value(scale || 1);
   const translateValue = new Animated.ValueXY(translate);
-  
-  const imageOpacity = scrollValueY.interpolate({
-    inputRange: [-SWIPE_CLOSE_OFFSET, 0, SWIPE_CLOSE_OFFSET],
-    outputRange: [0.5, 1, 0.5],
-  });
 
   const imagesStyles = getImageStyles(
     imageDimensions,
     translateValue,
-    scaleValue
+    // scaleValue
   );
-  
-  // Apply web zoom scaling
-  const imageStylesWithOpacity = {
-    ...imagesStyles,
-    // opacity: imageOpacity,
-    translateValue
-  };
 
   const styles = StyleSheet.create({
     listItem: {
@@ -96,7 +83,7 @@ const ImageItem = ({
       {(!loaded || !imageDimensions) && <ImageLoading />}
       <Animated.Image
         source={imageSrc}
-        style={[imageStylesWithOpacity]}
+        style={[imagesStyles]}
         onLoad={() => setLoaded(true)}
       />
     </View>
