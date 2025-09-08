e8060ebd0fba6720b2f4b9fcff5055623cfb1911
huyvowkm <huyvo@wakumo.vn>
2025-09-08 14:46:37 +0700

    fix: optimize image value

--- src/components/ImageItem/ImageItem.web.tsx
+++ src/components/ImageItem/ImageItem.web.tsx
@@ -23,8 +23,6 @@ import { ImageLoading } from "./ImageLoading";
 import useImageDimensions from "../../hooks/useImageDimensions";
 import { getImageStyles, getImageTransform } from "../../utils";
 
-const SWIPE_CLOSE_OFFSET = 75;
-const SWIPE_CLOSE_VELOCITY = 1.55;
 const SCREEN = Dimensions.get("screen");
 const SCREEN_WIDTH = SCREEN.width;
 const SCREEN_HEIGHT = SCREEN.height;
@@ -52,36 +50,24 @@ const ImageItem = ({
   containerStyle,
   style,
 }: Props) => {
-  const scrollViewRef = useRef<ScrollView>(null);
   const [loaded, setLoaded] = useState(false);
   const [scaled, setScaled] = useState(false);
   const imageDimensions = useImageDimensions(imageSrc);
   
   const [translate, scale] = getImageTransform(imageDimensions, SCREEN);
-  const scrollValueY = new Animated.Value(0);
-  const scaleValue = new Animated.Value(scale || 1);
   const translateValue = new Animated.ValueXY(translate);
-  const maxScale = scale && scale > 0 ? Math.max(1 / scale, 1) : 1;
   
-  const imageOpacity = scrollValueY.interpolate({
-    inputRange: [-SWIPE_CLOSE_OFFSET, 0, SWIPE_CLOSE_OFFSET],
-    outputRange: [0.5, 1, 0.5],
-  });
-
   const imagesStyles = getImageStyles(
     imageDimensions,
     translateValue,
-    scaleValue
   );
   
   // Apply web zoom scaling
-  const webZoomScale = scaled ? 2 : 1;
   const imageStylesWithOpacity = { 
     ...imagesStyles, 
-    opacity: imageOpacity,
+    opacity: 1,
     transform: [
       ...(imagesStyles.transform || []),
-      { scale: webZoomScale }
     ]
   };
 
