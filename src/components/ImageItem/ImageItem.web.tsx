9f3bf332127afdae1e8e7e00a1f3407b8927b74a
huyvowkm <huyvo@wakumo.vn>
2025-09-08 16:28:59 +0700

    test: remove list height

--- src/components/ImageItem/ImageItem.web.tsx
+++ src/components/ImageItem/ImageItem.web.tsx
@@ -112,7 +112,7 @@ const ImageItem = ({
 const styles = StyleSheet.create({
   listItem: {
     width: SCREEN_WIDTH,
-    height: SCREEN_HEIGHT,
+    // height: SCREEN_HEIGHT,
     backgroundColor: "red",
   },
   imageScrollContainer: {
