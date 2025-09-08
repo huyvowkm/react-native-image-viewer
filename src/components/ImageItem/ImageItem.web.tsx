3897ff90fb27ad882c5fd43d8a9d41d5d2ab8638
huyvowkm <huyvo@wakumo.vn>
2025-09-08 14:34:06 +0700

    fix: image opacity

--- src/components/ImageItem/ImageItem.web.tsx
+++ src/components/ImageItem/ImageItem.web.tsx
@@ -21,6 +21,7 @@ import {
 import { ImageSource } from "../../@types";
 import { ImageLoading } from "./ImageLoading";
 import useImageDimensions from "../../hooks/useImageDimensions";
+import { getImageStyles, getImageTransform } from "../../utils";
 
 const SWIPE_CLOSE_OFFSET = 75;
 const SWIPE_CLOSE_VELOCITY = 1.55;
@@ -53,12 +54,26 @@ const ImageItem = ({
 }: Props) => {
   const [loaded, setLoaded] = useState(false);
   const imageDimensions = useImageDimensions(imageSrc);
+  
+  const [translate, scale] = getImageTransform(imageDimensions, SCREEN);
+  const translateValue = new Animated.ValueXY(translate);
+
+  const imagesStyles = getImageStyles(
+    imageDimensions,
+    translateValue,
+  );
+  
+  const imageStylesWithOpacity = { 
+    ...imagesStyles, 
+    opacity: 1,
+  };
 
   return (
     <View style={[containerStyle]}>
       {(!loaded || !imageDimensions) && <ImageLoading />}
       <Animated.Image
         source={imageSrc}
+        style={[imageStylesWithOpacity]}
         onLoad={() => setLoaded(true)}
       />
     </View>
